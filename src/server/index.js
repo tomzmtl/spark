882ce9f6c8c093af0661773626c03f68bbcd2a4a
Thomas Andreo <thomas.andreo@gmail.com>
2018-03-19 22:47:49 -0400

    Serving handlebars file from server

---
+++ src/server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+
+
+const PORT = process.env.PORT || 3000;
+
+const expressHandlebars = require('express-handlebars');
+// const hbsHelpers = require('./helpers/handlebars');
+const hbs = expressHandlebars.create({
+  extname: '.hbs',
+  // helpers: hbsHelpers,
+});
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, './views'));
+
+app.use('/public', express.static(path.join(__dirname, '../../dist')));
+
+app.get('/favicon.ico', (req, res) => {
+  res.status(200).set({ 'Content-Type': 'image/x-icon' }).send()
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => {
+  console.log('Spark listening on port 3000!')
+});
