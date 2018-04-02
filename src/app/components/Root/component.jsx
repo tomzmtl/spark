18df813c85f8f8eb5fa5c50b17c864d362ccb7f7
Thomas Andreo <thomas.andreo@gmail.com>
2018-04-01 20:06:39 -0400

    Implemented redux-logger

--- src/app/components/Root/component.jsx
+++ src/app/components/Root/component.jsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { Provider as RouterProvider, createRouter } from 'fans-router';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import App from '../App/container';
 import reducer from '../../redux/reducers';
 import store from '../../redux/store';
@@ -11,8 +12,14 @@ import routes from '../../router/routes';
 import '../../../scss/app.scss';
 
 
+const logger = createLogger({
+  collapsed: true,
+  diff: true,
+  timestamp: false,
+});
+
 export default (
-  <Provider store={createStore(reducer, store, applyMiddleware(thunk))}>
+  <Provider store={createStore(reducer, store, applyMiddleware(thunk, logger))}>
     <RouterProvider router={createRouter(routes)}>
       <App />
     </RouterProvider>
